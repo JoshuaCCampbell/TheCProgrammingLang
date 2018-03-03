6ad950a2f40f37d3f97e198579a4d88fd8275c38
Joshua Campbell <joshuaccampbell@outlook.com>
2018-03-03 13:41:15 -0400

    1_13 complete

---
+++ Chapter_1/1_13/Main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LARGESTWORD 45 // Longest English word in major dicts
+
+#define MEMERR 1 // Error code for memory
+
+
+int main()
+{
+    int c;
+    int *wordlengths;
+
+    // Allocate memory for wordlengths array
+    // Exit if memory can't be allocated
+    wordlengths = malloc(sizeof(int) * LARGESTWORD);
+    if(!wordlengths)
+    {
+        fprintf(stderr, "Error allocating memory");
+        return MEMERR;
+    }
+
+    // Initialize values to 0
+    memset(wordlengths, 0, sizeof(int) * LARGESTWORD);
+
+    // Count the number of letters in a word
+    // Add to the running total
+    int numofletters = 0;
+    while((c = getchar()) != EOF)
+    {
+        if(c != ' ')
+        {
+            ++numofletters;
+        }
+        
+        if (c == ' ')
+        {
+            ++wordlengths[numofletters];
+            numofletters = 0;
+        }
+    }
+
+    ++wordlengths[numofletters];
+    
+    printf("\n");
+
+    int i;
+    for(i = 0; i < sizeof(wordlengths); ++i)
+    {
+        printf("%d letters words\t", i);
+        int j;
+        for(j = 0; j < wordlengths[i]; ++j)
+        {
+            printf("x");
+        }
+        printf("\n");
+    }
+    
+    free(wordlengths);
+    return 0;
+}
